aa9229bdeb13e915755a690b3fab6a26e3ea51cb
Ryan Choi <ryan.mingyu.choi@gmail.com>
2018-07-07 21:19:50 -0700

    More functionalities

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export class AddFishForm extends React.Component {
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+
+    createFish = e => {
+        e.preventDefault();
+        const fish = {
+            name: this.nameRef.value.value,
+            price: parseFloat(this.priceRef.value.value),
+            status: this.statusRef.value.value,
+            desc: this.descRef.value.value,
+            image: this.imageRef.value.valuef
+        }
+
+        this.props.addFish(fish);
+        e.currentTarget.reset();
+    }
+
+    render() {
+        return (
+            <form className="fish-edit" onSubmit={this.createFish}>
+                <input ref={this.nameRef} type="text" name="name" placeholder="Name" />
+                <input ref={this.priceRef} type="text" name="price" placeholder="Price" />
+                <select ref={this.statusRef} name="status">
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea ref={this.descRef} name="desc" placeholder="Desc"></textarea>
+                <input ref={this.imageRef} type="text" name="image" placeholder="Image" />
+                <button type="submit">+ Add Fish</button>
+            </form>
+        )
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,51 @@ import React from 'react';
 import { Header } from './Header';
 import { Order } from './Order';
 import { Inventory } from './Inventory';
+import { Fish } from './Fish';
+import sampleFishes from '../sample-fishes';
 
 export class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+        const fishes = { ...this.state.fishes };
+        fishes[`fish${Date.now()}`] = fish;
+        this.setState({ fishes });
+    };
+
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
+
+    addToOrder = (key) => {
+        const order = { ...this.state.order };
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        { 
+                            Object.keys(this.state.fishes).map(key => (
+                                <Fish
+                                    key={key}
+                                    index={key}
+                                    details={this.state.fishes[key]}
+                                    addToOrder={this.addToOrder}
+                                 />
+                            )) 
+                        }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
     }
